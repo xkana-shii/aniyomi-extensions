f9444455a63dd95bbb255452fa990a3ecd57143d
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-06-11 17:06:01 -0300

    Update BLZone.kt

--- src/en/blzone/src/eu/kanade/tachiyomi/animeextension/en/blzone/BLZone.kt
+++ src/en/blzone/src/eu/kanade/tachiyomi/animeextension/en/blzone/BLZone.kt
@@ -4,7 +4,12 @@ import android.app.Application
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
-import eu.kanade.tachiyomi.animesource.model.*
+import eu.kanade.tachiyomi.animesource.model.AnimeFilter
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.lib.filemoonextractor.FilemoonExtractor
 import eu.kanade.tachiyomi.lib.mixdropextractor.MixDropExtractor
