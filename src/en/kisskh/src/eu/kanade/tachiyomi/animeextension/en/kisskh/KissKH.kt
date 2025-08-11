a21f7b1dfd7cd5c6bd2a4df52d0419f4b505522d
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-08-11 07:35:43 -0300

    Revert "fix(en/kisskh): Fix video and subtitle requests & decryption, alternative domains & optimize code (#355)"
    This reverts commit d01a0c402a6a055d1934b7ca97bb9aef08350b67.

--- src/en/kisskh/src/eu/kanade/tachiyomi/animeextension/en/kisskh/KissKH.kt
+++ src/en/kisskh/src/eu/kanade/tachiyomi/animeextension/en/kisskh/KissKH.kt
@@ -1,8 +1,5 @@
 package eu.kanade.tachiyomi.animeextension.en.kisskh
 
-import androidx.preference.PreferenceScreen
-import eu.kanade.tachiyomi.animeextension.BuildConfig
-import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.AnimesPage
 import eu.kanade.tachiyomi.animesource.model.SAnime
@@ -11,16 +8,6 @@ import eu.kanade.tachiyomi.animesource.model.Track
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.network.awaitSuccess
-import eu.kanade.tachiyomi.util.parseAs
-import extensions.utils.LazyMutable
-import extensions.utils.addListPreference
-import extensions.utils.delegate
-import extensions.utils.getPreferencesLazy
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
@@ -33,9 +20,11 @@ import okhttp3.Headers
 import okhttp3.Request
 import okhttp3.Response
 
-class KissKH : AnimeHttpSource(), ConfigurableAnimeSource {
+class KissKH : AnimeHttpSource() {
 
     override val name = "KissKH"
+
+    override val baseUrl = "https://kisskh.co"
 
     override val lang = "en"
 
@@ -46,17 +35,6 @@ class KissKH : AnimeHttpSource(), ConfigurableAnimeSource {
         ignoreUnknownKeys = true
     }
 
-    private val preferences by getPreferencesLazy()
-
-    override var baseUrl: String
-        by preferences.delegate(PREF_DOMAIN_KEY, PREF_DOMAIN_DEFAULT)
-
-    private var subDecryptor by LazyMutable { SubDecryptor(client, headers, baseUrl) }
-
-    override val supportsRelatedAnimes = false
-
-    /* Popular Animes */
-
     override fun popularAnimeRequest(page: Int): Request =
         GET("$baseUrl/api/DramaList/List?page=$page&type=0&sub=0&country=0&status=0&order=1&pageSize=40")
 
@@ -65,39 +43,98 @@ class KissKH : AnimeHttpSource(), ConfigurableAnimeSource {
         return parsePopularAnimeJson(responseString)
     }
 
-    private fun parsePopularAnimeJson(jsonData: String): AnimesPage {
+    private fun parsePopularAnimeJson(jsonLine: String?): AnimesPage {
+        val jsonData = jsonLine ?: return AnimesPage(emptyList(), false)
         val jObject = json.decodeFromString<JsonObject>(jsonData)
-        val lastPage = jObject["totalCount"]?.jsonPrimitive?.int
-        val page = jObject["page"]?.jsonPrimitive?.int
-        val hasNextPage = if (lastPage != null && page != null) {
-            page < lastPage
-        } else {
-            false
+        val lastPage = jObject["totalCount"]!!.jsonPrimitive.int
+        val page = jObject["page"]!!.jsonPrimitive.int
+        val hasNextPage = page < lastPage
+        val array = jObject["data"]!!.jsonArray
+        val animeList = mutableListOf<SAnime>()
+        for (item in array) {
+            val anime = SAnime.create()
+            anime.title = item.jsonObject["title"]!!.jsonPrimitive.content
+            val animeId = item.jsonObject["id"]!!.jsonPrimitive.content
+            anime.setUrlWithoutDomain("$baseUrl/api/DramaList/Drama/$animeId?isq=false")
+            anime.thumbnail_url = item.jsonObject["thumbnail"]?.jsonPrimitive?.content ?: ""
+            animeList.add(anime)
         }
-        val animeList = jObject["data"]?.jsonArray?.mapNotNull { item ->
-            SAnime.create().apply {
-                title = item.jsonObject["title"]?.jsonPrimitive?.content ?: return@mapNotNull null
-                val animeId = item.jsonObject["id"]?.jsonPrimitive?.content ?: return@mapNotNull null
-                val titleURI = title.replace(titleUriRegex, "-")
-                url = "/Drama/$titleURI?id=$animeId"
-                thumbnail_url = item.jsonObject["thumbnail"]?.jsonPrimitive?.content
-            }
-        } ?: emptyList()
         return AnimesPage(animeList, hasNextPage)
     }
 
-    /* Latest */
+    // episodes
 
-    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/api/DramaList/List?page=$page&type=0&sub=0&country=0&status=0&order=2&pageSize=40")
-
-    override fun latestUpdatesParse(response: Response): AnimesPage {
+    override fun episodeListParse(response: Response): List<SEpisode> {
         val responseString = response.body.string()
-        return parseLatestAnimeJson(responseString)
+        return parseEpisodePage(responseString)
     }
 
-    private fun parseLatestAnimeJson(jsonData: String) = parsePopularAnimeJson(jsonData)
+    private fun parseEpisodePage(jsonLine: String?): List<SEpisode> {
+        val jsonData = jsonLine ?: return mutableListOf()
+        val jObject = json.decodeFromString<JsonObject>(jsonData)
+        val episodeList = mutableListOf<SEpisode>()
+        val array = jObject["episodes"]!!.jsonArray
+        val type = jObject["type"]!!.jsonPrimitive.content
+        val episodesCount = jObject["episodesCount"]!!.jsonPrimitive.int
+        for (item in array) {
+            val episode = SEpisode.create()
+            val id = item.jsonObject["id"]!!.jsonPrimitive.content
+            episode.episode_number = item.jsonObject["number"]!!.jsonPrimitive.float
+            val number = item.jsonObject["number"]!!.jsonPrimitive.content.replace(".0", "")
+            when {
+                type.contains("Anime") || type.contains("TVSeries") -> {
+                    episode.name = "Episode $number"
+                }
+                type.contains("Hollywood") && episodesCount == 1 || type.contains("Movie") -> {
+                    episode.name = "Movie"
+                }
+                type.contains("Hollywood") && episodesCount > 1 -> {
+                    episode.name = "Episode $number"
+                }
+            }
+            episode.setUrlWithoutDomain("$baseUrl/api/DramaList/Episode/$id.png?err=false&ts=&time=")
+            episodeList.add(episode)
+        }
+        return episodeList
+    }
 
-    /* Search */
+    // Video Extractor
+
+    override fun videoListParse(response: Response): List<Video> {
+        val id = response.request.url.toString()
+            .substringAfter("Episode/").substringBefore(".png")
+        return videosFromElement(response, id)
+    }
+
+    private val subDecryptor by lazy { SubDecryptor(client, headers, baseUrl) }
+
+    private fun videosFromElement(response: Response, id: String): List<Video> {
+        val jsonData = response.body.string()
+        val jObject = json.decodeFromString<JsonObject>(jsonData)
+        val videoList = mutableListOf<Video>()
+        val subData = client.newCall(GET("$baseUrl/api/Sub/$id")).execute().body.string()
+        val subj = json.decodeFromString<JsonArray>(subData)
+        val subList = mutableListOf<Track>()
+        for (item in subj) {
+            try {
+                val suburl = item.jsonObject["src"]!!.jsonPrimitive.content
+                val lang = item.jsonObject["label"]!!.jsonPrimitive.content
+
+                if (suburl.endsWith("txt")) {
+                    subList.add(subDecryptor.getSubtitles(suburl, lang))
+                } else {
+                    subList.add(Track(suburl, lang))
+                }
+            } catch (_: Error) {}
+        }
+        val videoUrl = jObject["Video"]!!.jsonPrimitive.content
+
+        videoList.add(Video(videoUrl, "FirstParty", videoUrl, subtitleTracks = subList, headers = Headers.headersOf("referer", "https://kisskh.me/", "origin", "https://kisskh.me")))
+
+        return videoList.reversed()
+    }
+
+    // Search
 
     override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request =
         GET("$baseUrl/api/DramaList/Search?q=$query&type=0")
@@ -107,43 +144,38 @@ class KissKH : AnimeHttpSource(), ConfigurableAnimeSource {
         return parseSearchAnimeJson(responseString)
     }
 
-    private fun parseSearchAnimeJson(jsonData: String): AnimesPage {
-        val animeList = json.decodeFromString<JsonArray>(jsonData).mapNotNull { item ->
-            SAnime.create().apply {
-                title = item.jsonObject["title"]?.jsonPrimitive?.content ?: return@mapNotNull null
-                val animeId = item.jsonObject["id"]?.jsonPrimitive?.content ?: return@mapNotNull null
-                val titleURI = title.replace(titleUriRegex, "-")
-                url = "/Drama/$titleURI?id=$animeId"
-                thumbnail_url = item.jsonObject["thumbnail"]?.jsonPrimitive?.content
-            }
+    private fun parseSearchAnimeJson(jsonLine: String?): AnimesPage {
+        val jsonData = jsonLine ?: return AnimesPage(emptyList(), false)
+        val array = json.decodeFromString<JsonArray>(jsonData)
+        val animeList = mutableListOf<SAnime>()
+        for (item in array) {
+            val anime = SAnime.create()
+            anime.title = item.jsonObject["title"]!!.jsonPrimitive.content
+            val animeId = item.jsonObject["id"]!!.jsonPrimitive.content
+            anime.setUrlWithoutDomain("$baseUrl/api/DramaList/Drama/$animeId?isq=false")
+            anime.thumbnail_url = item.jsonObject["thumbnail"]!!.jsonPrimitive.content
+            animeList.add(anime)
         }
         return AnimesPage(animeList, hasNextPage = false)
     }
 
-    /* Details */
-
-    override fun getAnimeUrl(anime: SAnime): String {
-        return baseUrl + anime.url
-    }
-
-    override fun animeDetailsRequest(anime: SAnime): Request {
-        val id = anime.url.substringAfter("id=").substringBefore("&")
-        return GET("$baseUrl/api/DramaList/Drama/$id?isq=false", headers)
-    }
+    // Details
 
     override fun animeDetailsParse(response: Response): SAnime {
         val responseString = response.body.string()
         return parseAnimeDetailsParseJson(responseString)
     }
 
-    private fun parseAnimeDetailsParseJson(jsonData: String): SAnime {
-        return SAnime.create().apply {
-            val jObject = json.decodeFromString<JsonObject>(jsonData)
-            jObject.jsonObject["title"]?.jsonPrimitive?.content?.let { title = it }
-            jObject.jsonObject["status"]?.jsonPrimitive?.content?.let { status = parseStatus(it) }
-            jObject.jsonObject["description"]?.jsonPrimitive?.content?.let { description = it }
-            jObject.jsonObject["thumbnail"]?.jsonPrimitive?.content?.let { thumbnail_url = it }
-        }
+    private fun parseAnimeDetailsParseJson(jsonLine: String?): SAnime {
+        val anime = SAnime.create()
+        val jsonData = jsonLine ?: return anime
+        val jObject = json.decodeFromString<JsonObject>(jsonData)
+        anime.title = jObject.jsonObject["title"]!!.jsonPrimitive.content
+        anime.status = parseStatus(jObject.jsonObject["status"]!!.jsonPrimitive.content)
+        anime.description = jObject.jsonObject["description"]!!.jsonPrimitive.content
+        anime.thumbnail_url = jObject.jsonObject["thumbnail"]!!.jsonPrimitive.content
+
+        return anime
     }
 
     private fun parseStatus(status: String?) = when {
@@ -152,140 +184,31 @@ class KissKH : AnimeHttpSource(), ConfigurableAnimeSource {
         else -> SAnime.COMPLETED
     }
 
-    /* Episodes */
+    // Latest
 
-    override fun episodeListRequest(anime: SAnime) = animeDetailsRequest(anime)
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/api/DramaList/List?page=$page&type=0&sub=0&country=0&status=0&order=2&pageSize=40")
 
-    override fun episodeListParse(response: Response): List<SEpisode> {
+    override fun latestUpdatesParse(response: Response): AnimesPage {
         val responseString = response.body.string()
-        return parseEpisodePage(responseString)
+        return parseLatestAnimeJson(responseString)
     }
 
-    private fun parseEpisodePage(jsonData: String): List<SEpisode> {
+    private fun parseLatestAnimeJson(jsonLine: String?): AnimesPage {
+        val jsonData = jsonLine ?: return AnimesPage(emptyList(), false)
         val jObject = json.decodeFromString<JsonObject>(jsonData)
-        val type = jObject["type"]?.jsonPrimitive?.content
-        val episodesCount = jObject["episodesCount"]?.jsonPrimitive?.int ?: 1
-        val episodeList = jObject["episodes"]?.jsonArray?.mapNotNull { item ->
-            val id = item.jsonObject["id"]?.jsonPrimitive?.content ?: return@mapNotNull null
-            val number = item.jsonObject["number"]?.jsonPrimitive?.content?.replace(".0", "") ?: "1"
-            SEpisode.create().apply {
-                url = id
-                item.jsonObject["number"]?.jsonPrimitive?.float?.let { episode_number = it }
-                when {
-                    type.isNullOrBlank() -> {
-                        name = "Video $number"
-                    }
-
-                    type.contains("Hollywood") && episodesCount == 1 || type.contains("Movie") -> {
-                        name = "Movie"
-                    }
-
-                    type.contains("Anime") || type.contains("TVSeries") ||
-                        type.contains("Hollywood") && episodesCount > 1 -> {
-                        name = "Episode $number"
-                    }
-                }
-            }
-        } ?: emptyList()
-        return episodeList
-    }
-
-    // Video Extractor
-    override suspend fun getVideoList(episode: SEpisode): List<Video> {
-        val kkey = requestVideoKey(episode.url)
-        val url = "$baseUrl/api/DramaList/Episode/${episode.url}.png?err=false&ts=&time=&kkey=$kkey"
-        val videoListRequest = GET(url, headers)
-        return client.newCall(videoListRequest)
-            .awaitSuccess()
-            .use { response ->
-                val id = response.request.url.toString()
-                    .substringAfter("Episode/").substringBefore(".png")
-                videosFromElement(response, id)
-            }
-    }
-
-    override fun videoListRequest(episode: SEpisode) = throw UnsupportedOperationException()
-    override fun videoListParse(response: Response) = throw UnsupportedOperationException()
-
-    private suspend fun videosFromElement(response: Response, id: String): List<Video> {
-        val jsonData = response.body.string()
-        val jObject = json.decodeFromString<JsonObject>(jsonData)
-        val videoUrl = jObject["Video"]?.jsonPrimitive?.content ?: return emptyList()
-
-        val kkey = requestSubKey(id)
-        val subData = client.newCall(GET("$baseUrl/api/Sub/$id?kkey=$kkey")).awaitSuccess().use { it.body.string() }
-
-        val subList = coroutineScope {
-            (runCatching { json.decodeFromString<JsonArray>(subData) }.getOrNull() ?: emptyList()).map { item ->
-                async {
-                    val suburl = item.jsonObject["src"]?.jsonPrimitive?.content ?: return@async null
-                    val lang = item.jsonObject["label"]?.jsonPrimitive?.content ?: "Unknown"
-
-                    runCatching {
-                        if (suburl.contains(".txt")) {
-                            subDecryptor.getSubtitles(suburl, lang)
-                        } else {
-                            Track(suburl, lang)
-                        }
-                    }.getOrNull()
-                }
-            }.awaitAll().filterNotNull()
+        val lastPage = jObject["totalCount"]!!.jsonPrimitive.int
+        val page = jObject["page"]!!.jsonPrimitive.int
+        val hasNextPage = page < lastPage
+        val array = jObject["data"]!!.jsonArray
+        val animeList = mutableListOf<SAnime>()
+        for (item in array) {
+            val anime = SAnime.create()
+            anime.title = item.jsonObject["title"]!!.jsonPrimitive.content
+            val animeId = item.jsonObject["id"]!!.jsonPrimitive.content
+            anime.setUrlWithoutDomain("$baseUrl/api/DramaList/Drama/$animeId?isq=false")
+            anime.thumbnail_url = item.jsonObject["thumbnail"]?.jsonPrimitive?.content ?: ""
+            animeList.add(anime)
         }
-
-        return listOf(
-            Video(
-                videoUrl,
-                "FirstParty",
-                videoUrl,
-                subtitleTracks = subList,
-                headers = Headers.headersOf("referer", "$baseUrl/", "origin", baseUrl),
-            ),
-        )
-    }
-
-    private suspend fun requestVideoKey(id: String): String {
-        val url = "${BuildConfig.KISSKH_API}$id&version=2.8.10"
-        return client.newCall(GET(url, headers)).awaitSuccess().use { it.parseAs<Key>().key }
-    }
-
-    private suspend fun requestSubKey(id: String): String {
-        val url = "${BuildConfig.KISSKH_SUB_API}$id&version=2.8.10"
-        return client.newCall(GET(url, headers)).awaitSuccess().use { it.parseAs<Key>().key }
-    }
-
-    @Serializable
-    data class Key(
-        val id: String,
-        val version: String,
-        val key: String,
-    )
-
-    override fun setupPreferenceScreen(screen: PreferenceScreen) {
-        screen.addListPreference(
-            key = PREF_DOMAIN_KEY,
-            title = "Preferred domain",
-            entries = DOMAIN_ENTRIES,
-            entryValues = DOMAIN_VALUES,
-            default = PREF_DOMAIN_DEFAULT,
-            summary = "%s",
-        ) {
-            baseUrl = it
-            subDecryptor = SubDecryptor(client, headers, baseUrl)
-        }
-    }
-
-    private val titleUriRegex by lazy { Regex("[^a-zA-Z0-9]") }
-
-    companion object {
-        private const val PREF_DOMAIN_KEY = "preferred_domain"
-        private val DOMAIN_ENTRIES = listOf(
-            "kisskh.ovh",
-            "kisskh.do",
-            "kisskh.co",
-            "kisskh.id",
-            "kisskh.la",
-        )
-        private val DOMAIN_VALUES = DOMAIN_ENTRIES.map { "https://$it" }
-        private val PREF_DOMAIN_DEFAULT = DOMAIN_VALUES[0]
+        return AnimesPage(animeList, hasNextPage)
     }
 }
