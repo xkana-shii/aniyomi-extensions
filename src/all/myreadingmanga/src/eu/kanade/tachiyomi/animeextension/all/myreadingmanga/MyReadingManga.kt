5fc39dff3125f91d082e1d649597a72b17980bd9
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-09-02 17:24:48 -0300

    Update MyReadingManga.kt

--- src/all/myreadingmanga/src/eu/kanade/tachiyomi/animeextension/all/myreadingmanga/MyReadingManga.kt
+++ src/all/myreadingmanga/src/eu/kanade/tachiyomi/animeextension/all/myreadingmanga/MyReadingManga.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.SharedPreferences
 import android.net.Uri
-import android.webkit.CookieManager
 import android.webkit.URLUtil
 import android.widget.Toast
 import androidx.preference.EditTextPreference
